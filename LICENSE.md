c632ce72400e9d8ddb3540e7e0ff3ef05f8b9291
panos <prpanagiotis@gmail.com>
2025-01-13 23:28:29 +0200

    rename structure

---
+++ LICENSE.md
@@ -0,0 +1,4 @@
+# PRISM Licensing
+
+## Core Library and CLI Application
+##TO DO: Add license
---
+++ src/cli/main.cpp
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+    // TODO: Implement CLI functionality
+    return 0;
+}
